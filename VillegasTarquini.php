466d51cf4a25956df55f35adfcfd6c06d8d8aeb9
OrnluX <ivantarquini91@gmail.com>
2023-11-22 18:48:55 -0300

    Modularización funciones menú principal

--- VillegasTarquini.php
+++ VillegasTarquini.php
@@ -329,91 +329,9 @@ function ordenarPartidas($coleccionPartidas){
   uasort($coleccionPartidas, 'compararJugadorPalabra'); //el método uasort ordena arreglos asociativos, manteniento la relación clave-valor luego del ordenamiento. El primer parámetro formal será el arreglo asociativo a ordenar. El segundo parámetro es una función personalizada de ordenamiento, que determina cómo se comparan los valores del arreglo asociativo.
   print_r($coleccionPartidas); //el método print_r se utiliza para mostrar información sobre variables de una manera legible. En este caso, la utilizamos en un arreglo indexado de arreglos asociativos. El método muestra la información de una manera estructurada y legible, indicando los índices, y los pares clave-valor.
 }
-/**************************************/
-/****** FUNCIONES COMPLEMENTARIAS******/
-/**************************************/
-/** Función diseñada para el menú principal. Cualquiera sea la tecla que ingrese el usuario se mostrará de nuevo el menú.
- * @return INT
-*/
-function presionarEnterContinuar(){
-  //STRING $tecla
-  //INT $opcion
-  escribirAmarillo("Presione ENTRAR para continuar");
-  $tecla = trim(fgets(STDIN));
-  if ($tecla != 9999) {
-    $opcion = 0;
-  }
-  return $opcion;
-}
-
-/** Función que verifica si un jugador ya jugó una partida con una palabra. Si ya existe una partida, la función devuelve true, de lo contrario devuelve false.
- * @param ARRAY $partidas
- * @param STRING $nombreJugador 
- * @param STRING $palabra
- * @return BOOLEAN
-*/
-function verificarPalabra($partidas, $nombreJugador, $palabra) {
-  //BOOLEAN $palabraUtilizada
-  //INT $indice
-  $palabraUtilizada = false;
-  $indice = 0;
-  while (!$palabraUtilizada && $indice < count($partidas)) {
-    if (($partidas[$indice]["jugador"]) == $nombreJugador && ($partidas[$indice]["palabraWordix"]) == $palabra) {
-      $palabraUtilizada = true;
-      break;
-    }
-    $indice ++;
-  }
-  return $palabraUtilizada;
-}
 
 /**************************************/
-/****** FUNCIONES MENU PRINCIPAL*******/
-/**************************************/
-
-/** Función correspondiente a la opción número 3 del menú principal. Pide al usuario un número de partida y llama a una función "mostrarPartida" pasándole por parámetro dicho número y la colección de partidas jugadas generadas en la sesión actual. Si la partida existe, muestra sus datos. Caso contrario pide nuevamente al usuario que ingrese un número válido.
- * @param ARRAY $coleccionPartidas
-*/
-function menuOpcion3($coleccionPartidas){
-  //INT $partidaNro
-  //BOOLEAN $existe
-  do {
-    echo "Ingrese el número de partida que desea visualizar: ";
-    $partidaNro = trim(fgets(STDIN));
-    echo "Buscando partida... \n\n";
-    $existe = mostrarPartida((intval($partidaNro)), $coleccionPartidas);
-  } while (!$existe);
-}
-
-/** Función correspondiente a la opción número 4 del menú principal. Consulta en la base de datos de las partidas existentes, la primer partida ganada por un jugador. En caso de existir esos datos, los muestra por pantalla, de lo contrario mostrará un mensaje por pantalla. 
- * @param ARRAY $coleccionPartidas 
-*/
-function menuOpcion4($coleccionPartidas) {
-  //STRING $jugador
-  //INT $indice
-  $jugador = solicitarJugador();
-  $indice = primerPartidaGanada($coleccionPartidas, $jugador);
-  if ($indice != (-1)) {
-    mostrarPartida(($indice+1), $coleccionPartidas);
-  }
-  else {
-    escribirRojo("El/la jugador/a " . $jugador . " no ganó ninguna partida");
-    echo " \n";
-  }
-}
-
-/**  Función correspondiente a la opción número 5 del menú principal. Muestra las estadísticas de un jugador. Partidas jugadas, ganadas y porcentaje de victorias.
- * @param ARRAY $coleccionPartidas
-*/
-function menuOpcion5($coleccionPartidas){
-  //STRING $jugador
-  $jugador = solicitarJugador();
-  estadisticasJugador($coleccionPartidas, $jugador);
-}
-
-
-/**************************************/
-/*********** PROGRAMA PRINCIPAL *******/
+/********** PROGRAMA PRINCIPAL ********/
 /**************************************/
 
 /*******DECLARACION DE VARIABLES*******/
--- funcionesMenu.php
+++ funcionesMenu.php
@@ -32,4 +32,82 @@ function opcionMenu1y2($palabrasWordix, &$partidasWordix, $opcionMenu) {
     } while ($palabraUtilizada);
   }
 
+  /** Función correspondiente a la opción número 3 del menú principal. Pide al usuario un número de partida y llama a una función "mostrarPartida" pasándole por parámetro dicho número y la colección de partidas jugadas generadas en la sesión actual. Si la partida existe, muestra sus datos. Caso contrario pide nuevamente al usuario que ingrese un número válido.
+ * @param ARRAY $coleccionPartidas
+*/
+function menuOpcion3($coleccionPartidas){
+    //INT $partidaNro
+    //BOOLEAN $existe
+    do {
+      echo "Ingrese el número de partida que desea visualizar: ";
+      $partidaNro = trim(fgets(STDIN));
+      echo "Buscando partida... \n\n";
+      $existe = mostrarPartida((intval($partidaNro)), $coleccionPartidas);
+    } while (!$existe);
+  }
+  
+  /** Función correspondiente a la opción número 4 del menú principal. Consulta en la base de datos de las partidas existentes, la primer partida ganada por un jugador. En caso de existir esos datos, los muestra por pantalla, de lo contrario mostrará un mensaje por pantalla. 
+   * @param ARRAY $coleccionPartidas 
+  */
+  function menuOpcion4($coleccionPartidas) {
+    //STRING $jugador
+    //INT $indice
+    $jugador = solicitarJugador();
+    $indice = primerPartidaGanada($coleccionPartidas, $jugador);
+    if ($indice != (-1)) {
+      mostrarPartida(($indice+1), $coleccionPartidas);
+    }
+    else {
+      escribirRojo("El/la jugador/a " . $jugador . " no ganó ninguna partida");
+      echo " \n";
+    }
+  }
+  
+  /**  Función correspondiente a la opción número 5 del menú principal. Muestra las estadísticas de un jugador. Partidas jugadas, ganadas y porcentaje de victorias.
+   * @param ARRAY $coleccionPartidas
+  */
+  function menuOpcion5($coleccionPartidas){
+    //STRING $jugador
+    $jugador = solicitarJugador();
+    estadisticasJugador($coleccionPartidas, $jugador);
+  }
+
+  /**************************************/
+/****** FUNCIONES COMPLEMENTARIAS******/
+/**************************************/
+/** Función diseñada para el menú principal. Cualquiera sea la tecla que ingrese el usuario se mostrará de nuevo el menú.
+ * @return INT
+*/
+function presionarEnterContinuar(){
+    //STRING $tecla
+    //INT $opcion
+    escribirAmarillo("Presione ENTRAR para continuar");
+    $tecla = trim(fgets(STDIN));
+    if ($tecla != 9999) {
+      $opcion = 0;
+    }
+    return $opcion;
+  }
+  
+  /** Función que verifica si un jugador ya jugó una partida con una palabra. Si ya existe una partida, la función devuelve true, de lo contrario devuelve false.
+   * @param ARRAY $partidas
+   * @param STRING $nombreJugador 
+   * @param STRING $palabra
+   * @return BOOLEAN
+  */
+  function verificarPalabra($partidas, $nombreJugador, $palabra) {
+    //BOOLEAN $palabraUtilizada
+    //INT $indice
+    $palabraUtilizada = false;
+    $indice = 0;
+    while (!$palabraUtilizada && $indice < count($partidas)) {
+      if (($partidas[$indice]["jugador"]) == $nombreJugador && ($partidas[$indice]["palabraWordix"]) == $palabra) {
+        $palabraUtilizada = true;
+        break;
+      }
+      $indice ++;
+    }
+    return $palabraUtilizada;
+  }
+
   ?>
